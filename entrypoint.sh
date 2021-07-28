8f55102f50ca9834dc40a88d5a1c433445535469
slickpockets <slickpocketsroberts@gmail.com>
2021-07-28 20:19:15 +0800

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-*/1 * * * * /usr/bin/python3 /main.py >> /var/log/cron.log 2>&1
-*/1 * * * * date >> /var/log/cron.log 2>&1
+*/5 * * * * /usr/bin/python3 /main.py >> /var/log/cron.log 2>&1
+*/5 * * * * date >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt

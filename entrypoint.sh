9f6f50505eef90997ab2c7972ce34a60efac8699
slickpockets <slickpocketsroberts@gmail.com>
2021-07-28 16:12:26 +0800

    updatE

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-*/5 * * * * python3 /main.py >> /var/log/cron.log 2>&1
-#* * * * * /run.sh >> /var/log/cron.log 2>&1
+*/5 * * * * /usr/bin/python3 /main.py >> /var/log/cron.log 2>&1
+*/5 * * * * date >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 import datetime
 
 from dotenv import dotenv_values
-config = dotenv_values('.env')
+config = dotenv_values('/.env')
 
 TOKEN = config['DIGITALOCEAN_ACCESS_TOKEN']
 DOMAIN = config['DOMAIN']
